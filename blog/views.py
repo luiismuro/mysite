48e39bcb34f8e7cef237f79a27d0045b95d8b850
Luis Muro <luiismuro@gmail.com>
2025-06-14 18:42:05 -0400

    Atualizando templates html e realizando migrações

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from blog.models import Post
 
-# Create your views here.
+class PostView(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    context_object_name = 'post_list'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post-detail.html'    
