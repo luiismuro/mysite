83489cdba0dc4f05d5f37f0f86e722ad741b986d
Luis Muro <luiismuro@gmail.com>
2025-06-23 19:44:42 -0400

    remove post listview e post detail, mantendo o post view do hello world

--- blog/views.py
+++ blog/views.py
@@ -5,15 +5,3 @@ from django.views import generic
 class PostView(generic.View):
     def get(self, request, *args, **kwargs):
         return HttpResponse('Hello world')
-
-from blog.models import Post
-
-class PostView(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
-    context_object_name = 'post_list'
-
-class PostDetail(generic.DetailView):
-    model = Post
-    template_name = 'post-detail.html'    
-
